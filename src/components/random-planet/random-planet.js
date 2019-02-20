5489907e4dc66a9945ce960b00521abdb9f63155
Vasteman <assaultrifle@list.ru>
2019-02-20 17:02:26 +0400

    fix loading and error indicators

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import SwapiService from '../../services/swapi-service'
 import Spinner from '../spinner'
+import ErrorIndicator from '../error-indicator'
 
 import './random-planet.css';
 
@@ -20,27 +21,41 @@ export default class RandomPlanet extends Component {
   }
 
   onPlanetLoaded = (planet) => {
+    console.log('загрузил')
     this.setState({
       planet,
-      loading: false
+      loading: false,
+      error: false
     })
   }
 
+  onError = (err) => {
+    this.setState({
+      error: true,
+      loading: false
+    })
+  }
   updatePlanet() {
-    const id = Math.floor(Math.random() * 25) + 2;
+    console.log('послал запрос')
+    let id = Math.floor(Math.random() * 25) + 2;
     this.SwapiService.getPlanet(id)
       .then(this.onPlanetLoaded)
+      .catch(this.onError)
   }
 
 
   render() {
-    const { planet, loading } = this.state;
+    const { planet, loading, error } = this.state;
 
+    const hasData = !(error || loading);
+
+    const errorMessage = error ? <ErrorIndicator /> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = !loading ? <PlanetView planet={planet} /> : null;
+    const content = hasData ? <PlanetView planet={planet} /> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
         {spinner}
         {content}
       </div>
@@ -49,13 +64,14 @@ export default class RandomPlanet extends Component {
   }
 }
 
-const PlanetView = ({planet}) => {
+const PlanetView = ({ planet }) => {
   const { id, name,
     population,
     rotationPeriod,
     diameter } = planet;
   return (
     <React.Fragment>
+      {console.log('рисую')}
       <img className="planet-image"
         src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
         alt={`${name}.jpg`} />
