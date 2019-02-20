a6a5bb633ab039c467e6f6c05bf0318572a69852
Vasteman <assaultrifle@list.ru>
2019-02-20 13:50:33 +0400

    Add NetworkService

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
-const getResource = async (url) => {
-  const res = await fetch(url);
-  const body = await res.json();
-  return body;
+
+
+class SwapiService {
+
+  _apiBase = 'https://swapi.co/api';
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}` +
+        `, code ${res.status}`)
+    }
+    const body = await res.json();
+    return body;
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource('/people/')
+    return res.results;
+  }
+
+  getPersone(id) {
+    return this.getResource(`/people/${id}/`)
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource('/planets/')
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}/`)
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource('/starships/')
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}/`)
+  }
 }
 
-getResource('https://swapi.co/api/people/1/')
-  .then((body) => {
-    console.log(body)
+const swapi = new SwapiService();
+swapi.getAllStarships().then((planets) => {
+  planets.forEach((p) => {
+    console.log(p.name)
   })
+})
