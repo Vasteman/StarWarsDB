dc6d630935ad1b8cc19f95547927bf010a74b3c3
Vasteman <assaultrifle@list.ru>
2019-02-21 15:45:11 +0400

    Realized pages

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
@@ -20,7 +21,7 @@ export default class App extends Component {
   onServiceChange = () => {
     this.setState(({ swapiService }) => {
       const Service = swapiService instanceof SwapiService ?
-                        DummySwapiService : SwapiService;
+        DummySwapiService : SwapiService;
       return {
         swapiService: new Service()
       };
@@ -32,15 +33,21 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService} >
-          <div className="stardb-app">
-            <Header onServiceChange={this.onServiceChange} />
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={this.onServiceChange} />
 
-            <RandomPlanet />
-            <PeoplePage />
-            <PlanetsPage />
-            <StarshipsPage />
+              <RandomPlanet />
 
-          </div>
+              <Route  path="/" 
+                      render={()=><h2>Welcome to StarWars DataBase!</h2>}
+                      exact />
+              <Route path="/people" component={PeoplePage} />
+              <Route path="/planets" component={PlanetsPage} />
+              <Route path="/starships" component={StarshipsPage} />
+
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
