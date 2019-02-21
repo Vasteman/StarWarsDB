af9ab6efc304d82a26c2f21893d13c3af5cd0813
Vasteman <assaultrifle@list.ru>
2019-02-21 16:48:04 +0400

    Finish?

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
@@ -7,21 +6,37 @@ import ErrorBoundry from '../error-boundry';
 import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
 
-import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
+import {
+  PeoplePage,
+  PlanetsPage,
+  StarshipsPage,
+  LoginPage,
+  SecretPage } from '../pages';
+
 import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import StarshipDetails from '../sw-components/starship-details';
+
 export default class App extends Component {
 
   state = {
-    swapiService: new SwapiService()
+    swapiService: new SwapiService(),
+    isLoggedIn: false
+  };
+
+  onLogin = () => {
+    this.setState({
+      isLoggedIn: true
+    });
   };
 
   onServiceChange = () => {
     this.setState(({ swapiService }) => {
       const Service = swapiService instanceof SwapiService ?
-        DummySwapiService : SwapiService;
+                        DummySwapiService : SwapiService;
       return {
         swapiService: new Service()
       };
@@ -30,21 +45,45 @@ export default class App extends Component {
 
   render() {
 
+    const { isLoggedIn } = this.state;
+
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService} >
           <Router>
             <div className="stardb-app">
               <Header onServiceChange={this.onServiceChange} />
-
               <RandomPlanet />
 
-              <Route  path="/" 
-                      render={()=><h2>Welcome to StarWars DataBase!</h2>}
-                      exact />
-              <Route path="/people" component={PeoplePage} />
-              <Route path="/planets" component={PlanetsPage} />
-              <Route path="/starships" component={StarshipsPage} />
+              <Switch>
+                <Route path="/"
+                       render={() => <h2>Welcome to StarDB</h2>}
+                       exact />
+                <Route path="/people/:id?" component={PeoplePage} />
+                <Route path="/planets" component={PlanetsPage} />
+                <Route path="/starships" exact component={StarshipsPage} />
+                <Route path="/starships/:id"
+                       render={({ match }) => {
+                         const { id } = match.params;
+                         return <StarshipDetails itemId={id} />
+                       }}/>
+
+                <Route
+                  path="/login"
+                  render={() => (
+                    <LoginPage
+                      isLoggedIn={isLoggedIn}
+                      onLogin={this.onLogin}/>
+                  )}/>
+
+                <Route
+                  path="/secret"
+                  render={() => (
+                    <SecretPage isLoggedIn={isLoggedIn} />
+                  )}/>
+
+                <Route render={() => <h2>Page not found</h2>} />
+              </Switch>
 
             </div>
           </Router>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './header.css';
 
@@ -8,18 +8,24 @@ const Header = ({ onServiceChange }) => {
     <div className="header d-flex">
       <h3>
         <Link to="/">
-          StarWarsDB
+          StarDB
         </Link>
       </h3>
       <ul className="d-flex">
         <li>
-          <Link to='/people'>People</Link>
+          <Link to="/people/">People</Link>
         </li>
         <li>
-          <Link to='/planets'>Planets</Link>
+          <Link to="/planets/">Planets</Link>
         </li>
         <li>
-          <Link to='/starships'>Starships</Link>
+          <Link to="/starships/">Starships</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/secret">Secret</Link>
         </li>
       </ul>
 
